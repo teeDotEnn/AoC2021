b41235b97d9eb266f7734981ab89e5002c5443af
Tim Nigh <nigh.timothy@gmail.com>
2021-12-03 14:41:38 -0500

    added position behaviour

--- day2/Position.cs
+++ day2/Position.cs
@@ -1,6 +1,23 @@
 class Position{
+  
   private int depth;
   private int horizontalPos;
 
   public int FinalPos => depth*horizontalPos;
+
+  public void ChangePosition((string direction, int magnitude) vector){
+    if(vector.direction.Equals("forward")){
+      horizontalPos += vector.magnitude;
+    }
+    if(vector.direction.Equals("down")){
+      depth += vector.magnitude;
+    }
+    if(vector.direction.Equals("up")){
+      depth -= vector.magnitude;
+    }
+  }
+
 }
+
+
+
